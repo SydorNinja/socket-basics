efec8f01c95e0f8114c3c002c3a44e98847cd6e7
Robert Sorkin <robasorkin@gmail.com>
2016-10-12 17:09:44 +0300

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,23 @@
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
 
 socket.on('message', function(message) {
+	var timestamp = message.timestamp;
+	var timestampMoment = moment.utc(timestamp);
 	console.log('New message:');
 	console.log(message.text);
-	jQuery('.messages').append('<p>' + message.text + '<p/>');
+	console.log(timestampMoment.local().format('h:mm a'));
+
+	jQuery('.messages').append('<p>' + '<strong>' + timestampMoment.local().format('h:mm a') + ': ' + '</strong>' + message.text + '<p/>');
 });
 
 
 var $form = jQuery('#message-form');
 
-$form.on('submit', function (event) {
+$form.on('submit', function(event) {
 	event.preventDefault();
 
 	$message = $form.find('input[name=message]');
